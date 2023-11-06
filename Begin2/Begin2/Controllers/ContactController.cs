abce77676e17df718ca22971b9b3aa628800a286
GoranBakic <goran.bakic2001@gmail.com>
2023-11-06 18:35:38 +0100

    Finish exercise 3

--- Begin2/Begin2/Controllers/ContactController.cs
+++ Begin2/Begin2/Controllers/ContactController.cs
@@ -1,5 +1,6 @@
 ﻿using Begin2.Models;
 using Begin2.Services;
+using System.Net.Http;
 using System.Web.Http;
 
 
@@ -17,6 +18,15 @@ namespace Begin2.Controllers
         public Contact[] Get()
         {
             return contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
         }
     }
 }
